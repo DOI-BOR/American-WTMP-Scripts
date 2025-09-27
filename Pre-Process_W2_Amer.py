c446ed0316e69d3c14b22ce7759890b8efa66332
bsaenz <BenSae4598@geiconsultants.com>
2025-09-27 12:23:31 -0700

    Scripting support for W2 inital gate settings, W2 starting after 1May

--- Pre-Process_W2_Amer.py
+++ Pre-Process_W2_Amer.py
@@ -16,6 +16,8 @@ reload(fpp)
 import create_balance_flow_jython as cbfj
 reload(cbfj)
 
+shutter_links = ['init_elev_shutter_1','init_elev_shutter_2','init_elev_shutter_3']
+
 def computeAlternative(currentAlternative, computeOptions):
     currentAlternative.addComputeMessage("Computing ScriptingAlternative:" + currentAlternative.getName())
     currentAlternative.addComputeMessage('\n')
@@ -82,18 +84,35 @@ def computeAlternative(currentAlternative, computeOptions):
                         dss_type='PER-AVER', period='1DAY',cpart='ZEROS',fpart='ZEROS')
     DSS_Tools.create_constant_dss_rec(currentAlternative, rtw, forecast_dss, constant=0.0, what='flow', 
                         dss_type='PER-AVER', period='1HOUR',cpart='ZEROS',fpart='ZEROS')
+    print('Making ELEV-INITIAL-GATE records...')
+    DSS_Tools.create_constant_dss_rec(currentAlternative, rtw, forecast_dss, constant=401.0, what='elev', 
+                        dss_type='PER-AVER', period='1DAY',cpart='ELEV-INITIAL-GATE',fpart='FULL-HEIGHT')
+    DSS_Tools.create_constant_dss_rec(currentAlternative, rtw, forecast_dss, constant=362.0, what='elev', 
+                        dss_type='PER-AVER', period='1DAY',cpart='ELEV-INITIAL-GATE',fpart='1-OUT')
+    DSS_Tools.create_constant_dss_rec(currentAlternative, rtw, forecast_dss, constant=336.0, what='elev', 
+                        dss_type='PER-AVER', period='1DAY',cpart='ELEV-INITIAL-GATE',fpart='2-OUT')
+    DSS_Tools.create_constant_dss_rec(currentAlternative, rtw, forecast_dss, constant=307.0, what='elev', 
+                        dss_type='PER-AVER', period='1DAY',cpart='ELEV-INITIAL-GATE',fpart='ALL-OUT')
 
-    # update W2 interative control files for restart date
-    # this functionality is already in the w2 plugin - did you check to see if folsom iterative compute option is checked??
-    fpp.update_W2_Folsom_iterative_restart_date(rtw,model_dir)
+    # find initial shutter elevations
+    locations_obj = currentAlternative.getInputDataLocations()
+    shutter_objs = DSS_Tools.organizeLocations(currentAlternative, locations_obj, shutter_links, return_dss_paths=False)
+    w2_elevs = fpp.get_initial_shutter_positions(rtw, currentAlternative, computeOptions, shutter_objs)
+    print('Initial W2 shutter elevs:',w2_elevs)
+
+    # update W2 interative control files for restart date - wait this functionality is already in the w2 plugin - did you check to see if folsom iterative compute option is checked??
+    # also update the inital gate elevations in the w2_con file
+    fpp.update_W2_Folsom_iterative_restart_date_and_shutters(rtw,model_dir,w2_elevs)
 
     # if this is a non-iterative simulation, put the correct ensemble number into the 'Set Guess' file 
-    # -- edit, 2025-01-27, B. Saenz.  We want to set this correct ensemble for all runs, as it tells also
-    # tells the iterative version of Folsom W2 to start with a parituclar schedule, so save time
     #if 'FixedATSP' in model_name:
-
+    # -- edit, 2025-01-27, B. Saenz.  We want to set the  correct ensemble for all runs, as it also
+    # tells the iterative version of Folsom W2 to start with a parituclar schedule, to save time if you have a good guess
     w2run_base,_ = os.path.split(run_dir)
     fpp.update_W2_Folsom_iterative_schedule_number(w2run_base,model_dir)
+
+    # The W2 selective file must be modified depending on the start gate to get the auto shutter to work correctly
+    fpp.modify_w2_selective_start_date(rtw,os.path.join(model_dir,'w2_selective.npt'))
 
     if target_temp_write and folsom_outlets:
         return True
