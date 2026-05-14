f5f0e40372a43fb430769ff8997fc82d3a1a7e99
Loney <dloney@usbr.gov>
2026-05-14 12:56:15 -0600

    Update to generalize paths

--- OutputLink_W2_Folsom-DownstreamAmer.py
+++ OutputLink_W2_Folsom-DownstreamAmer.py
@@ -344,11 +344,11 @@ def computeAlternative(currentAlternative, computeOptions):
     study_root = computeOptions.getRunDirectory()  # returns watershed directory
     
     # Split the path and strip off the scripts, alternative-group, and run folder to obtain the WAT directory
-    study_wat = study_root.split(os.sep)
-    study_wat = os.sep.join(study_wat[:-3])
+    study_main = study_root.split(os.sep)
+    study_main = os.sep.join(study_main[:-3])
     
     # Create the directory to the WAT simulation group
-    study_wat = study_wat + os.sep + 'wat' + os.sep + 'simGroups'
+    study_wat = study_main + os.sep + 'wat' + os.sep + 'simGroups'
     
     # Get the contents of the directory
     all_groups = os.listdir(study_wat)
@@ -358,9 +358,6 @@ def computeAlternative(currentAlternative, computeOptions):
     
     # Remove backup files not caught by the previous filter
     forecast_groups = [x for x in forecast_groups if '.bak' not in x]
-    
-    for x in forecast_groups:
-        currentAlternative.addComputeMessage("group name: " +  x)
     
     # Strip off the file extensions from each name
     forecast_groups = [x.split(".fsimgrp")[0] for x in forecast_groups]
@@ -379,17 +376,19 @@ def computeAlternative(currentAlternative, computeOptions):
             break
     
     # Remove the group from the simulation name
-    currentAlternative.addComputeMessage("final group name: " +  x)
     alt_name = sim_name.split(group_name)[0]
     
-    # Remove the final dash that was not removed
+    # Remove the dash from the alternative name
     alt_name = alt_name[:-1]
-    currentAlternative.addComputeMessage("Alternative name: " +  alt_name)  # log message  
+    
+    # Construct the new simulation name that is the disk name
+    alt_name_underscore = alt_name.replace(' ', '_')
+    sim_name_underscore = alt_name_underscore + '-' + group_name
 
     ### Adjust the model name based on the alternative name ###
     # Get the current run directory
     run_dir = computeOptions.getRunDirectory()
-    
+
     # Start with the base model name. This is an assumed convention. 
     model_name = "W2 Folsom"  # base Folsom forecast model (iterative w/ bypass)
     
@@ -404,7 +403,7 @@ def computeAlternative(currentAlternative, computeOptions):
         model_name += " NoBypass"
     
     ### Setup the paths ###
-    w2_run_dir = os.path.join(run_dir,'cequal-w2','Folsom',model_name)
+    w2_run_dir = os.path.join(study_main, 'runs', sim_name_underscore, 'CeQual-W2', 'Folsom', model_name)
     str_csv = os.path.join(w2_run_dir,'str_br1.csv')
     vol_file = os.path.join(w2_run_dir,'VOLUME_WB1.OPT')
     
@@ -447,16 +446,8 @@ def computeAlternative(currentAlternative, computeOptions):
 
     # Write out target temps and schedule number
     # ------------------------------------------------------------------------------------
-    run_dir = computeOptions.getRunDirectory() # runs/model_alt_name/scripting
-    model_name = 'W2 Folsom'  # base Folsom forecast model (iterative w/ bypass)
-    if "FixedATSP" in run_dir:
-        model_name += " FixedATSP"
-    if "NoBypass" in run_dir:
-        model_name += " NoBypass"
-    model_dir = fpp.model_dir_from_run_dir(run_dir,'Folsom',model_name)
-    run_base_dir,_ = os.path.split(run_dir)
-    model_run_dir_Folsom = os.path.join(run_base_dir,'CeQual-W2','Folsom',model_name)
-    shared_dir = os.path.join(fpp.study_dir_from_run_dir(run_dir),'shared')
+    model_run_dir_Folsom = w2_run_dir
+    shared_dir = os.path.join(study_main,'shared')
     forecast_dss = os.path.join(shared_dir,'WTMP_American_forecast.dss')
     
     # read AutoRunTempLog.opt and find last schedule used
--- Post-Process_W2_Fol-Nat.py
+++ Post-Process_W2_Fol-Nat.py
@@ -29,17 +29,17 @@ def computeAlternative(currentAlternative, computeOptions):
     startyear_str = starttime_str[5:9]
     year = int(startyear_str)
 
-### Determine the alternative name ###
+    ### Determine the alternative name ###
     # This step is necessary to set the model up correctly based on American configurations
     # Get the current folder
     study_root = computeOptions.getRunDirectory()  # returns watershed directory
     
     # Split the path and strip off the scripts, alternative-group, and run folder to obtain the WAT directory
-    study_wat = study_root.split(os.sep)
-    study_wat = os.sep.join(study_wat[:-3])
+    study_main = study_root.split(os.sep)
+    study_main = os.sep.join(study_main[:-3])
     
     # Create the directory to the WAT simulation group
-    study_wat = study_wat + os.sep + 'wat' + os.sep + 'simGroups'
+    study_wat = study_main + os.sep + 'wat' + os.sep + 'simGroups'
     
     # Get the contents of the directory
     all_groups = os.listdir(study_wat)
@@ -49,9 +49,6 @@ def computeAlternative(currentAlternative, computeOptions):
     
     # Remove backup files not caught by the previous filter
     forecast_groups = [x for x in forecast_groups if '.bak' not in x]
-    
-    for x in forecast_groups:
-        currentAlternative.addComputeMessage("group name: " +  x)
     
     # Strip off the file extensions from each name
     forecast_groups = [x.split(".fsimgrp")[0] for x in forecast_groups]
@@ -70,17 +67,19 @@ def computeAlternative(currentAlternative, computeOptions):
             break
     
     # Remove the group from the simulation name
-    currentAlternative.addComputeMessage("final group name: " +  x)
     alt_name = sim_name.split(group_name)[0]
     
-    # Remove the final dash that was not removed
+    # Remove the dash from the alternative name
     alt_name = alt_name[:-1]
-    currentAlternative.addComputeMessage("Alternative name: " +  alt_name)  # log message  
+    
+    # Construct the new simulation name that is the disk name
+    alt_name_underscore = alt_name.replace(' ', '_')
+    sim_name_underscore = alt_name_underscore + '-' + group_name
 
     ### Adjust the model name based on the alternative name ###
     # Get the current run directory
     run_dir = computeOptions.getRunDirectory()
-    
+
     # Start with the base model name. This is an assumed convention. 
     model_name = "W2 Folsom"  # base Folsom forecast model (iterative w/ bypass)
     
@@ -95,11 +94,9 @@ def computeAlternative(currentAlternative, computeOptions):
         model_name += " NoBypass"
     
     ### Setup the paths ###
-    model_dir = fpp.model_dir_from_run_dir(run_dir,'Folsom',model_name)
-    run_base_dir,_ = os.path.split(run_dir)
-    model_run_dir_Folsom = os.path.join(run_base_dir,'CeQual-W2','Folsom',model_name)
+    model_dir = os.path.join(study_main, 'runs', sim_name_underscore, 'CeQual-W2', 'Folsom', model_name)
     targt_temp_npt_filepath = os.path.join(model_dir,'TargetSchedulesA.npt') # overwrite what's there    
-    shared_dir = os.path.join(fpp.study_dir_from_run_dir(run_dir),'shared')
+    shared_dir = os.path.join(study_main,'shared')
     forecast_dss = os.path.join(shared_dir,'WTMP_American_forecast.dss')
     schedule_csv = os.path.join(model_dir,'SchedulesA.csv')
 
